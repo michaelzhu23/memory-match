1520a83336ae5ca3503bb012400f39093537abb9
Michael Zhu <mzhu23@gmail.com>
2020-03-05 16:52:02 -0800

    Memory Match Reset Game Feature 6 completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,30 @@
 function displayStats(){
-  document.getElementById("gamesPlayed").textContent = gamesPlayed;
-  document.getElementById("attempts").textContent = attempts;
-  document.getElementById("accuracy").textContent = calculateAccuracy(attempts, matches) + "%";
+  document.getElementById("gamesPlayedId").textContent = gamesPlayed;
+  document.getElementById("attemptsId").textContent = attempts;
+  document.getElementById("accuracyId").textContent = calculateAccuracy(attempts, matches) + "%";
 }
 
 function calculateAccuracy(attempts, matches){
+  if(!attempts){
+    return 0;
+  }
   return Math.trunc((matches / attempts) * 100);
 }
+
+function resetGame(){
+  matches = 0;
+  attempts = 0;
+  gamesPlayed++;
+  displayStats();
+  resetCards();
+  document.querySelector(".modal-overlay").classList.add("hidden");
+}
+
+function resetCards(){
+  var hiddenCards = document.querySelectorAll("div.card-back");
+  for(var i = 0; i < hiddenCards.length; i++){
+    hiddenCards[i].classList.remove("hidden");
+  }
+}
+
+document.getElementById("resetButton").addEventListener("click", resetGame);
