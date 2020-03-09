37b0ca4a27cdb0df26b395da41847150588bad15
Michael Zhu <mzhu23@gmail.com>
2020-03-09 16:02:22 -0700

    added custom images, sounds and fonts. still need to finish timer function

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,34 @@ var matches = 0;
 var attempts = 0;
 var gamesPlayed = 0;
 var main = document.getElementById("gameCards");
+var time;
+var timerText = document.getElementById("timer");
+var buzzerSound = document.getElementById("buzzer");
+var swishSound = document.getElementById("swish");
+var backboardSound = document.getElementById("backboard");
+var cheeringSound = document.getElementById("cheering");
+
 
 main.addEventListener("click", handleClick);
 function handleClick(event){
   if (event.target.className.indexOf("card-back") === -1) {
     return;
+  }
+  if(!time){
+    time = 60;
+    var interval = setInterval(function () {
+      time--;
+      if (time) {
+        timerText.textContent = "0:" + time;
+        if (time < 10) {
+          timerText.textContent = "0:0" + time;
+        }
+      } else {
+        timerText.textContent = "0:00";
+        buzzerSound.play();
+        clearInterval(interval);
+      }
+    }, 1000)
   }
   event.target.className += " hidden";
   if(!firstCardClicked){
@@ -22,6 +45,7 @@ function handleClick(event){
     secondCardClasses = secondCardClicked.previousElementSibling.className;
     main.removeEventListener("click", handleClick);
       if(firstCardClasses === secondCardClasses){
+        swishSound.play();
         main.addEventListener("click", handleClick);
         firstCardClicked = null;
         secondCardClicked = null;
@@ -29,9 +53,11 @@ function handleClick(event){
         attempts++;
         displayStats();
         if(matches === maxMatches){
+          cheeringSound.play();
           document.querySelector(".modal-overlay").classList.remove("hidden");
         }
       } else {
+        backboardSound.play();
         setTimeout(function(){
           firstCardClicked.classList.remove("hidden");
           secondCardClicked.classList.remove("hidden");
